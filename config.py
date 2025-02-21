f986bd61bc9226a7a4430f59e3ef6a1f81705453
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-21 13:03:23 +0530

    config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-cloudinary.config(
-    cloud_name="dsuolsjio",
-    api_key="684851764112618",
-    api_secret="BAYsU8wkFPUKEOB1-6xNKZdo2N8"
-)
+cloudinary.config( 
+  cloud_name = "dsuolsjio",  
+  api_key = "684857164112618",  
+  api_secret = "BAYsU8wkFPUKEOB1-6xNKZdo2N8"  )

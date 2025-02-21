dc8960bae6fa48780ab73ac76835f9947b9c1281
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-22 01:04:30 +0530

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+import os
 
-cloudinary.config( 
-  cloud_name = "dsuolsjlo",  
-  api_key = "684851764112618",  
-  api_secret = "BAYsU8wkFPUKEOB1-6xNKZdo2N8"  
-)
+class Config:
+    SECRET_KEY = "my_super_secret_key"  # अपनी Secret Key डालें
+    MYSQL_HOST = "127.0.0.1"  # या आपका MySQL सर्वर एड्रेस
+    MYSQL_USER = "root"  # MySQL यूज़र नेम
+    MYSQL_PASSWORD = "mahadev@5757"  # MySQL पासवर्ड
+    MYSQL_DB = "exam_db"  # डेटाबेस का नाम

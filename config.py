62380fde2a40d2ecf080ce146e7dc75b383966fc
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-23 00:37:41 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-class Config:
+import mysql.connector
+
+db = mysql.connector.connect(
     SECRET_KEY = "81d744702ecb4accadeba0f85bec5f61"
     MYSQL_HOST = "karan-LOQ-15IAX9"
     MYSQL_USER = "root"
     MYSQL_PASSWORD = "mahadev@5757"
     MYSQL_DB = "exam_db"
+
+)
+cursor = db.cursor()

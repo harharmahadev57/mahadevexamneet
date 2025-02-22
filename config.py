cc23e231cfd98e60e4601f50a7228292861edd5a
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-22 20:27:18 +0530

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
-import os
-from dotenv import 
-load_dotenv
-
-load_dotenv()
-
 class Config:
-    SECRET_KEY = os.getenv("81d744702ecb4accadeba0f85bec5f61")
-    MYSQL_HOST = os.getenv("karan-LOQ-15IAX9")
-    MYSQL_USER = os.getenv("root")
-    MYSQL_PASSWORD = os.getenv("mahadev@5757")
-    MYSQL_DB = os.getenv("exam_db")
+    SECRET_KEY = "81d744702ecb4accadeba0f85bec5f61"
+    MYSQL_HOST = "karan-LOQ-15IAX9"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "mahadev@5757"
+    MYSQL_DB = "exam_db"

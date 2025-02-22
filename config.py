ac99fb918c600ccaaa0d9c39e619ed8fd2d9ec71
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-22 10:40:33 +0530

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import os
-
 class Config:
-    SECRET_KEY = "my_super_secret_key"  # अपनी Secret Key डालें
-    MYSQL_HOST = "127.0.0.1"  # या आपका MySQL सर्वर एड्रेस
-    MYSQL_USER = "root"  # MySQL यूज़र नेम
-    MYSQL_PASSWORD = "mahadev@5757"  # MySQL पासवर्ड
-    MYSQL_DB = "exam_db"  # डेटाबेस का नाम
+    SECRET_KEY = "81d744702ecb4accadeba0f85bec5f61"
+    MYSQL_HOST = "karan-LOQ-15IAX9"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "mahadev@5757"
+    MYSQL_DB = "exam_db"

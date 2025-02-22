426758ed66f2459049ecc86d7d2b24834c85c2c1
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-22 10:54:27 +0530

    config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_DB = os.getenv("MYSQL_DB")
+    SECRET_KEY = os.getenv("81d744702ecb4accadeba0f85bec5f61")
+    MYSQL_HOST = os.getenv("karan-LOQ-15IAX9")
+    MYSQL_USER = os.getenv("root")
+    MYSQL_PASSWORD = os.getenv("mahadev@5757")
+    MYSQL_DB = os.getenv("exam_db")

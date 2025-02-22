a8c911918ee7c770fdec03ad68001c0e53131b8f
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-22 11:05:57 +0530

    db.sql

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE exam_db;
+
+USE exam_db;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);

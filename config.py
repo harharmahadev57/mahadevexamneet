93bd8255d7cadcff30b564400b3ed43de94ed465
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-23 15:39:08 +0530

    config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ SECRET_KEY = "7d07b2e7bb3c4d36d43a8dde0dc4b7c7f18f3d6600c09f9e3f0d1acf7a095437"
 
 db = mysql.connector.connect(
     
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "mahadev@5757"
-    MYSQL_DB = "exam_db"
+    MYSQL_HOST="localhost"
+    MYSQL_USER="root"
+    MYSQL_PASSWORD="mahadev@5757"
+    MYSQL_DB="exam_db"
 
 )
 cursor = db.cursor()

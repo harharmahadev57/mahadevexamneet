cccdbc29fc85d5d6bda58ae952540ee71b424e88
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-23 15:17:46 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import mysql.connector
 
+
+SECRET_KEY = "7d07b2e7bb3c4d36d43a8dde0dc4b7c7f18f3d6600c09f9e3f0d1acf7a095437"
+
 db = mysql.connector.connect(
-    SECRET_KEY = "7d07b2e7bb3c4d36d43a8dde0dc4b7c7f18f3d6600c09f9e3f0d1acf7a095437"
+    
     MYSQL_HOST = "karan-LOQ-15IAX9"
     MYSQL_USER = "root"
     MYSQL_PASSWORD = "mahadev@5757"

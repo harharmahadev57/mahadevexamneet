9bb676ead7b795903aaed1b76723a4f0e34441dc
harharmahadev57 <karanvachhani47@gmail.com>
2025-02-24 13:50:11 +0530

    script.js

--- static/script.js
+++ static/script.js
@@ -55,3 +55,25 @@ function adminLogin() {
         }
     });
 }
+
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// CORS Enable करें
+app.use(cors({
+    origin: '*', // सभी origins को allow करने के लिए (*)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.get('/', (req, res) => {
+    res.send('CORS is enabled!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
